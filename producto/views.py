fcb01414933216240be984672afa64672d6e0aae
mariaescalante <marescram3@alum.us.es>
2023-11-15 01:03:20 +0100

    feat: details of products

--- producto/views.py
+++ producto/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Producto
+from django.shortcuts import render, get_object_or_404
 
 def lista_productos(request):
     productos = Producto.objects.all()
     return render(request, 'lista_productos.html', {'productos': productos})
+
+def detalles_producto(request, producto_id):
+    producto = get_object_or_404(Producto, pk=producto_id)
+    return render(request, 'detalles_producto.html', {'producto': producto})

cd814633d8835bb1413f8267f65f3b2c314cd289
mariaescalante <marescram3@alum.us.es>
2023-11-18 20:42:26 +0100

    feat: sales products added

--- producto/models.py
+++ producto/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from oferta.models import Oferta
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     marca = models.CharField(max_length=100)
@@ -10,7 +12,10 @@ class Producto(models.Model):
     unidades = models.IntegerField(default=0)
     capacidad_viajar = models.IntegerField(default=0)
     capacidad_dormir = models.IntegerField(default=0)
+    oferta = models.ForeignKey(Oferta, on_delete=models.CASCADE, null=True, default=None)
 
+    def precio_rebajado(self):
+        return float(self.precio) - float(self.oferta.porcentaje)/100. * float(self.precio)
 
     def str(self):
         return self.nombre

53bfbb8d80dd81da75cfe9bc1c50e74ab86f9d0c
jualeomad <jualeomad@alum.us.es>
2023-11-18 13:53:43 +0100

    feat: realizar_pedido post
    Se realiza un post en el carrito tras pulsar REALIZAR PEDIDO y pasa a la siguiente fase de compra (no implementada). En el post se actualiza el carrito en base de datos con las nuevas cantidades que solo estaban sincronizadas con localStorage.

--- cesta/urls.py
+++ cesta/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import agregar_al_carrito, ver_carrito
+from .views import agregar_al_carrito, ver_carrito, realizar_pedido
 
 urlpatterns = [
     path('agregar-al-carrito/<int:producto_id>/', agregar_al_carrito, name='agregar_al_carrito'),
     path('carrito/', ver_carrito, name='ver_carrito'),
+    path('realizar_pedido/', realizar_pedido, name='realizar_pedido')
 ]
--- cesta/views.py
+++ cesta/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Producto, Carrito, ItemCarrito
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
 
 
@@ -23,3 +23,30 @@ def ver_carrito(request):
     items = carrito.items.all()
 
     return render(request, 'carrito.html', {'carrito': carrito, 'items': items})
+
+
+def realizar_pedido(request):
+    if request.method == "POST":
+        # Procesar los datos del formulario
+        user = request.user
+        carrito, created = Carrito.objects.get_or_create(usuario=user)
+        items = []
+
+        for key, value in request.POST.items():
+            if key.startswith("cantidad_"):
+                producto_id = key.replace("cantidad_", "")
+                cantidad = int(value)
+                producto = Producto.objects.get(pk=producto_id)
+                item, created = ItemCarrito.objects.get_or_create(carrito=carrito, producto=producto)
+                item.cantidad = cantidad
+                item.save()
+                items.append(item)
+
+        # Actualizar el carrito con los nuevos elementos
+        carrito.items.set(items)
+        carrito.save()
+
+        return HttpResponse("Pedido realizado con éxito!")
+    else:
+
+        return HttpResponse("Error al realizar el pedido!")

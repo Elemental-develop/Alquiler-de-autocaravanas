0093c13f52716a1db6622df4ccf16ae611eee5a1
mariaescalante <marescram3@alum.us.es>
2023-11-20 19:04:14 +0100

    feat: catalogue's organization by categories

--- producto/views.py
+++ producto/views.py
@@ -5,10 +5,13 @@ from oferta.models import Oferta
 from django.shortcuts import render, get_object_or_404
 
 def lista_productos(request):
-    
+        
+    categoria = request.GET.get('categoria', '')
     productos = Producto.objects.all()
-    ofertas = Oferta.objects.all()
-      
+    
+    if categoria != '':
+        productos = Producto.objects.filter(categoria=categoria)
+
     return render(request, 'lista_productos.html', {'productos': productos})
 
 def detalles_producto(request, producto_id):

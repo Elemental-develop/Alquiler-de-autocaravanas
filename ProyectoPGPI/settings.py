6faf5a1e9129449519f82c428157759c64dbec3d
jualeomad <jualeomad@alum.us.es>
2023-12-12 17:32:15 +0100

    fix: stripe api keys

--- ProyectoPGPI/settings.py
+++ ProyectoPGPI/settings.py
@@ -142,8 +142,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ProyectoPGPI/static')]
 
-STRIPE_PUBLIC_KEY = 'pk_test_51OH5liE1VZogj5Oj0dHBqa4m8iUUnJjHs0LghaN5wIcPLFKBPCUckQhWNOKJdI3uBX1jL1jIrlFOwYhiUcIbVDv300Veb3Ep6W'
-STRIPE_SECRET_KEY = 'sk_test_51OH5liE1VZogj5OjKwwCo8ZJMfiC3JFxOdgUGlPT5u2N4o9h9kOExlDoUGkerZugsu6rdS36Zmdt2l1e54QprGme00e08qLRsQ'
+STRIPE_PUBLIC_KEY = 'pk_test_51OMYEWAmk1TRRd75YDioT4DCqR7omSwdILyQwjDEMeY1G8IgmzdCgwmpQC2KufiYFOiBYztLgWOzW3JX6GymkFKn00eyx9LCFZ'
+STRIPE_SECRET_KEY = 'sk_test_51OMYEWAmk1TRRd75cF5EGFuV9BMhSUH6nHNbGXax5iZsPLxRavaRYGvlJ1pgTlPDpMOBGHJyDIpn6wU8vpsed1cB00pELllbrJ'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
